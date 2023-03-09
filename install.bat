545be86569d232396154f089f9282f5ee1e7d626
Yùjǐn Lín <yujinlin0224@outlook.com>
2023-03-10 06:25:16 +0800

    backup when install in `install.bat`

--- install.bat
+++ install.bat
@@ -1,17 +1,25 @@
 @echo off
 
-if exist "%ProgramFiles%\MPC-HC" (
-  copy /y "%~dp0\toolbar*.svg" "%ProgramFiles%\MPC-HC"
-)
+setlocal EnableDelayedExpansion
 
-if exist "%ProgramFiles(x86)%\MPC-HC" (
-  copy /y "%~dp0\toolbar*.svg" "%ProgramFiles(x86)%\MPC-HC"
-)
+set Filenames=2
+set Filenames[1]=toolbar.svg
+set Filenames[2]=toolbar_dark.svg
 
-if exist "%ProgramFiles%\K-Lite Codec Pack\MPC-HC" (
-  copy /y "%~dp0\toolbar*.svg" "%ProgramFiles%\K-Lite Codec Pack\MPC-HC"
-)
+set ProgramDirPaths=5
+set ProgramDirPaths[1]=%ProgramFiles%\MPC-HC
+set ProgramDirPaths[2]=%ProgramFiles(x86)%\MPC-HC
+set ProgramDirPaths[3]=%ProgramFiles%\K-Lite Codec Pack\MPC-HC
+set ProgramDirPaths[4]=%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC
+set ProgramDirPaths[5]=%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64
 
-if exist "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64" (
-  copy /y "%~dp0\toolbar*.svg" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64"
+for /l %%i in (1,1,%ProgramDirPaths%) do (
+  if exist "!ProgramDirPaths[%%i]!" (
+    for /l %%j in (1,1,%Filenames%) do (
+      if exist "!ProgramDirPaths[%%i]!\!Filenames[%%j]!" (
+        copy /y "!ProgramDirPaths[%%i]!\!Filenames[%%j]!" "!ProgramDirPaths[%%i]!\!Filenames[%%j]!.bak"
+      )
+      copy /y "%~dp0\!Filenames[%%j]!" "!ProgramDirPaths[%%i]!"
+    )
+  )
 )
